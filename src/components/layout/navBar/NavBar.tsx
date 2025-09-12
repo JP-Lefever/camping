4cba2a66f08ba8ad1f7ad4e429ea4bdaf1f1c38b
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-09-12 18:40:36 +0200

    feat: add menu nav & styles

--- src/components/layout/navBar/NavBar.tsx
+++ src/components/layout/navBar/NavBar.tsx
@@ -1,11 +1,19 @@
+"use client"
 import styles from "./navBar.module.css"
-
+import {useState} from "react";
 import Image from "next/image";
-import {Menu, Phone, CircleQuestionMark  } from "lucide-react"
+import {Menu, Phone, CircleQuestionMark, X  } from "lucide-react"
 import Link from "next/link";
+import dataNav from "@/assets/data/navBar.json"
 
 
 export default function NavBar() {
+
+    const [openMenu, setOpenMenu] = useState<boolean>(false)
+
+    const handleMenu = () => {
+        setOpenMenu(!openMenu)
+    }
 
     return(
         <>
@@ -15,16 +23,29 @@ export default function NavBar() {
                         <Image className={styles.logo} src={"/images/logo.jpg"} alt={"logo camping, petite cabane"} width={100} height={100} />
                     </figure>
                     <article>
-                        <Menu size={56} color={"#fc841c"}/>
+                        <Menu className={styles.buttonMenu} size={56} color={"white"} onClick={handleMenu}/>
+
+                            <ul className={`${styles.ul} ${openMenu ? styles.open : styles.close }`}>
+                                <li className={styles.headMenu}>
+                                    <p className={styles.p}>Menu</p>
+                                    <X className={styles.button} onClick={handleMenu} size={48}/>
+                                </li>
+                            {dataNav.map((item)=>
+                                <li className={styles.li} key={item.id}>
+                                    <Link className={styles.linkMenu} href={item.link}>{item.label}</Link>
+                                </li>
+                            )}
+                            </ul>
+
                     </article>
                 </section>
                 <section>
                     <article>
-                        <CircleQuestionMark size={48} color={"#fc841c"}/>
+                        <CircleQuestionMark size={48} color={"white"}/>
                         <Link className={styles.link} href="#"> Nous contacter </Link>
                     </article>
                     <article>
-                        <Phone/>
+                        <Phone color={"white"} />
                         <p>01.01.01.01.01</p>
                     </article>
                 </section>
