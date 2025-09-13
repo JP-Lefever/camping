0dab68148631b6fbb4ac50d7b4f01fd47311806e
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-09-13 11:45:09 +0200

    feat: add header component with image & H1Ã

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,15 @@
+import styles from "./header.module.css"
+
+
+export default function Header(){
+
+
+    return (
+        <>
+        <header className={styles.header} >
+            <h1>Glamping Spot</h1>
+            <h2>Envie de glam'der ?</h2>
+        </header>
+        </>
+    )
+}
