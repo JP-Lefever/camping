d1db207b59ecd24c02567a3dbae9de2aa27409e8
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-09-16 15:03:16 +0200

    feat: add camping page & data camping

---
+++ src/assets/data/camping.ts
@@ -0,0 +1,47 @@
+export const presCamping = {
+    titlePres : "Glamping Spot",
+    subtitle : "Bienvenue à Glamping Spot",
+    slogan : "Votre parenthèse nature dans le Lot",
+    description : `
+Niché au cœur d’un cadre verdoyant et paisible, Glamping Spot vous invite à vivre une expérience unique alliant confort et nature. Ici, le charme authentique du camping rencontre le raffinement du glamping, pour des séjours inoubliables en plein air.
+
+Notre domaine se situe dans le Lot, une région riche en paysages préservés, villages pittoresques et trésors culturels. Que vous soyez amateurs de randonnées, passionnés d’histoire ou simplement en quête de détente, vous trouverez ici l’endroit idéal pour vous ressourcer.
+
+**Un camping pensé pour votre confort**
+
+À Glamping Spot, nous mettons un point d’honneur à offrir un séjour qui rime avec bien-être et sérénité :
+
+- Hébergements insolites et confortables, parfaitement intégrés à la nature.  
+- Espaces communs conviviaux pour partager des moments chaleureux.  
+- Services et équipements pensés pour rendre votre séjour agréable et pratique.
+
+**Entre découverte et détente**
+
+Profitez de votre passage pour explorer les merveilles du Lot :
+
+- Les célèbres villages classés comme Saint-Cirq-Lapopie ou Rocamadour.  
+- La vallée du Lot et ses balades au fil de l’eau.  
+- Les marchés locaux où saveurs et authenticité se rencontrent.
+
+Et quand vous préférez rester sur place, laissez-vous porter par l’ambiance apaisante du camping, bercé par la nature environnante.
+  `
+}
+
+export const infoCamping = {
+    title : "Info Pratiques",
+    address : `**Adresse :** Glamping spot, 1 rue du paradis, 46100 Figeac`,
+    opening : `**Date d'ouverture :** 01/04/2025`,
+    closing : `**Date de fermeture :** 30/09/2025`,
+    tel : `**Téléphone :** 01.01.01.01.01`,
+    mail : `**Mail :** glamping@mail.com`,
+    access : `**Accès :** Route, gare proche, transport public `,
+    pmr : `**Accessibilité PMR :** Hébergements et sanitaires adaptés`,
+    pitches : `**Nombre d'emplacement :** 58`,
+    taxes : `**Taxe de séjour :** X€ par personne/nuit `,
+    payment : `**Paiement :** CB, espèces, chèques vacances`,
+    checkMH : `**Heure arrivée locatif :** 17h`,
+    checkoutMh : `**Heure départ locatif :** 10h`,
+    checkPitch : `**Heure arrivée emplacement :** 12h`,
+    checkoutPitch : `**Heure départ emplacement :** 12h`,
+
+}
