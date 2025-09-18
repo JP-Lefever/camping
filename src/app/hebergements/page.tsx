a1b260e5b23fffe9a1ab7cd0da04c21d41446f3e
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2025-09-18 11:15:34 +0200

    feat: add cardLocation to lacations page & styles

--- src/app/hebergements/page.tsx
+++ src/app/hebergements/page.tsx
@@ -1,3 +1,7 @@
+import styles from "./page.module.css";
+import {presCamping } from "@/assets/data/camping";
+import {dataLocations } from "@/assets/data/locations";
+import CardLocation from "@/components/cardLocation/CardLocation";
 
 
 export default function LocationsPage(){
@@ -5,6 +9,21 @@ export default function LocationsPage(){
 
     return (
         <>
+            <section  >
+                <article className={styles.article}>
+                    <h1>{presCamping.titlePres}</h1>
+                </article>
+                <article >
+                    <h2 className={styles.h2}>{"Nos hébergements"}</h2>
+                    <h3 className={styles.h3}>{"Votre prochaine destination vous ouvre ses portes pour des vacances inoubliables."}</h3>
+                </article>
+                <article className={styles.articleLocations}>
+                    {dataLocations.map(location =>
+                        <CardLocation key={location.id} dataLocation={location} />
+                    )}
+
+                </article>
+            </section>
         </>
     )
 }
