a70b25aed7bd82ab9c637cb6e92423efac10f09a
LEFEVER JEAN-PHILIPPE <lefever.jp@hotmail.fr>
2026-01-31 13:02:57 +0100

    feat: add styles

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -11,17 +11,15 @@ export default function servicesPage() {
             <article className={styles.article}>
                 <h1>{presCamping.titlePres}</h1>
             </article>
-            <article >
-                <h2>{"Nos services"}</h2>
+            <article className={styles.articleInfo} >
+                <h2 className={styles.h2}>{"Nos services"}</h2>
                 <h3>{"Pour des vacances encore plus confortables"}</h3>
-            <ul>
+            <ul className={styles.ul}>
             {services.map((s) => (
-                <li key={s.id}>
-                    <Image src={s.image} alt={s.label} width={40} height={40} />
-                    <div>
-                        <h2>{s.label}</h2>
-                        <p>{s.detail}</p>
-                    </div>
+                <li className={styles.li} key={s.id}>
+                    <Image src={s.image} alt={s.label} width={60} height={60} />
+                    <h2>{s.label}</h2>
+                    <p>{s.detail}</p>
                 </li>
             ))}
             </ul>
